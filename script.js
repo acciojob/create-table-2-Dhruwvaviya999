970e6630e97789aff7c9f1ec1e3b4639c20d9bb8
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-21 11:48:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+// let rn = prompt("Input number of rows");
+// let cn = prompt("Input number of columns");
+
+let  rn = 3;
+let cn = 4;
+
+if(parseInt(rn) <= 0 || parseInt(cn) <= 0){
+	alert("Please enter a valid row and column number.");
+}
+
+const table = document.querySelector("#myTable");
+
 function createTable() {
     //Write your code here
-  
+	for(let i=0; i<rn; i++){
+		table.insertRow(i);
+	}
+
+	let tableRows = document.querySelectorAll("tr");
+	tableRows.forEach((row, rowIdx)=>{
+		for(let colIndx=0; colIndx<cn; colIndx++){
+			let cell = row.insertCell(colIndx);
+			cell.innerText = `Row-${rowIdx} Column-${colIndx}`;
+		}
+	});
+	
 }
