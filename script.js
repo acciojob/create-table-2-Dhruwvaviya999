331eff2491fcaea8b888c041559cda0dc1ff4309
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-04-01 02:21:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// let rn = prompt("Input number of rows");
-// let cn = prompt("Input number of columns");
-
-let rn = 3;
-let cn = 4;
+let rn = prompt("Input number of rows");
+let cn = prompt("Input number of columns");
 
 if(parseInt(rn) <= 0 || parseInt(cn) <= 0){
 	alert("Please enter a valid row and column number.");
